2010a144fc3da2c6ec7c029bbd3613554d7a1f4b
Michal Tesler <michal.tesler@gmail.com>
2022-07-01 08:47:06 +0200

    Added variables (website, path, driver)

---
+++ data_collection.py
@@ -0,0 +1,6 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+
+website = 'adamchoi.co.uk/teamgoals/detailed'
+path = 'C:\dev\Python\chromedriver_win32'
+driver = webdriver.Chrome(path)
