69d725fa72b325dacc998f42def09468d435d3e4
Michal Tesler <michal.tesler@gmail.com>
2022-07-04 06:50:39 +0200

    Added click method to show all games without division into home and away matches

--- data_collection.py
+++ data_collection.py
@@ -5,4 +5,8 @@ path = '/Users/micha/Downloads/chromedriver_win32/chromedriver.exe'
 driver = webdriver.Chrome(path)
 driver.get(website)
 
+all_matches_button = driver.find_element_by_xpath(
+    '//label[@analytics-event="All matches"]')
+all_matches_button.click()
+
 driver.quit()
