805b9a3abb309011f023ef3aa9cf2d7902286d56
Michal Tesler <michal.tesler@gmail.com>
2022-07-05 13:42:40 +0200

    Added .gitignore

--- data_collection.py
+++ data_collection.py
@@ -9,4 +9,9 @@ all_matches_button = driver.find_element_by_xpath(
     '//label[@analytics-event="All matches"]')
 all_matches_button.click()
 
+matches = driver.find_elements_by_tag_name('tr')
+
+for match in matches:
+    print(match.text)
+
 driver.quit()
