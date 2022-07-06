1f7829c772a1cca4d772b73b11933e24943657ad
Michal Tesler <michal.tesler@gmail.com>
2022-07-06 07:37:25 +0200

    Split date from a table to separate lists

--- data_collection.py
+++ data_collection.py
@@ -6,11 +6,21 @@ path = '/Users/micha/Downloads/chromedriver_win32/chromedriver.exe'
 driver = webdriver.Chrome(path)
 driver.get(website)
 
-all_matches_button = driver.find_element('xpath', '//label[@analytics-event="All matches"]')
+all_matches_button = driver.find_element(
+    'xpath', '//label[@analytics-event="All matches"]')
 all_matches_button.click()
 
 matches = driver.find_elements(By.TAG_NAME, 'tr')
+
+date = []
+home_team = []
+result = []
+away_team = []
+
 for match in matches:
-    print(match.text)
+    date.append(match.find_element('xpath', './td[1]').text)
+    home_team.append(match.find_element('xpath', './td[2]').text)
+    result.append(match.find_element('xpath', './td[3]').text)
+    away_team.append(match.find_element('xpath', './td[4]').text)
 
 driver.quit()
