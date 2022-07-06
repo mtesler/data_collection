8ae11830829e345855e1584284cb8d6c51880b1a
Michal Tesler <michal.tesler@gmail.com>
2022-07-06 07:04:49 +0200

    Added part about extracting data from a table

--- data_collection.py
+++ data_collection.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 website = 'https://adamchoi.co.uk/teamgoals/detailed'
 path = '/Users/micha/Downloads/chromedriver_win32/chromedriver.exe'
 driver = webdriver.Chrome(path)
 driver.get(website)
 
-all_matches_button = driver.find_element_by_xpath(
-    '//label[@analytics-event="All matches"]')
+all_matches_button = driver.find_element('xpath', '//label[@analytics-event="All matches"]')
 all_matches_button.click()
 
-matches = driver.find_elements_by_tag_name('tr')
-
+matches = driver.find_elements(By.TAG_NAME, 'tr')
 for match in matches:
     print(match.text)
 
